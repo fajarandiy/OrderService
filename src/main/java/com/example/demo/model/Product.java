ec402bdab05b4109ef8d35a9ad23db00e620eddf
zugenk <zugenk@yahoo.com>
2019-09-24 14:08:42 +0700

    Product

---
+++ src/main/java/com/example/demo/model/Product.java
@@ -0,0 +1,27 @@
+package com.example.demo.model;
+
+public class Product {
+	String name;
+	String description;
+	double price;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public double getPrice() {
+		return price;
+	}
+
+}
